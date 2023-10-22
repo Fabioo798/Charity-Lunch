ddc6c15b26fc44fd67774b3e32b263821f16e46a
Fabio <fabiodn.798@gmail.com>
2023-10-22 11:36:18 +0200

    🧱 Create layout component

---
+++ restaurant/src/app/infrastructure/layout/layout.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LayoutComponent } from './layout.component';
+import { HeaderComponent } from '../header/header.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [LayoutComponent, HeaderComponent],
+    });
+    fixture = TestBed.createComponent(LayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ restaurant/src/app/infrastructure/layout/layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MenuItems } from 'src/interfaces/interfaces';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent {}
