a2138b15ad302be70c775e84ecb96ab74cb56abd
Fabio <fabiodn.798@gmail.com>
2023-10-22 11:37:43 +0200

    ♻️ Update routing

--- restaurant/src/app/app-routing.module.ts
+++ restaurant/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderDishComponent } from './order-dish/order-dish.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'order', component: OrderDishComponent },
+  { path: 'history', component: OrderHistoryComponent },
+  { path: 'recipes', component: RecipeComponent },
+  { path: 'storage', component: OrderDishComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
