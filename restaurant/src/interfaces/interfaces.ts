211d0581c5ffc2c61db2de25b428e2eda0b411a7
Fabio <fabiodn.798@gmail.com>
2023-10-22 11:26:49 +0200

    🏷️  Create Interface file

---
+++ restaurant/src/interfaces/interfaces.ts
@@ -0,0 +1,28 @@
+export interface Dish {
+  id?: string;
+  name: string;
+  ingredients: Ingredient[];
+}
+
+export interface Ingredient {
+  id?: number;
+  name: string;
+  quantity: number;
+}
+
+export interface Order {
+  id: number;
+  dishId: number;
+  timestamp: Date;
+}
+
+export interface KitchenOrderResponse {
+  results: {
+    data: Dish;
+  };
+}
+
+export interface MenuItems {
+  label: string;
+  path: string;
+}
