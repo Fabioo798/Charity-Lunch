cdc0219ebe7e7d66d5e62d36e4bc33c5909de88c
Fabio <fabiodn.798@gmail.com>
2023-10-24 10:12:44 +0200

    ✅  create order mongo repo

---
+++ kitchen/src/order/infrastructure/order.mongo.repo.test.ts
@@ -0,0 +1,89 @@
+import { OrderModel } from "../../server/domain/order.schema.js";
+import Order, { OrderState } from "../domain/order.model.js";
+import OrderMongoRepo from "./order.mongo.repo.js";
+
+jest.mock('../../server/domain/order.schema');
+
+const mockModel = {
+  find: jest.fn(),
+  create: jest.fn(),
+  findByIdAndUpdate: jest.fn(),
+  findById: jest.fn(),
+  findByIdAndDelete: jest.fn(),
+} as unknown as typeof OrderModel;
+
+const mockOrder = {
+  id: '2',
+  dish: {
+   id: '1',
+   name: 'test',
+   ingredients: ['peper', 'test2', 'test3'],
+  },
+  timeStamp: Date.arguments,
+  state: OrderState.InProgress,
+} as Order;
+let popValue: unknown;
+
+const mockPopulateExec = () => ({
+  populate: jest.fn().mockImplementation(() => ({
+    populate: jest.fn().mockImplementation(() => ({
+      exec: jest.fn().mockResolvedValue(popValue),
+    })),
+  })),
+});
+
+describe('Given the OrderMongoRepo', () => {
+  const repo = new OrderMongoRepo(mockModel);
+
+  describe('When it is called', () => {
+    test('Then it should be instantiated', () => {
+      expect(repo).toBeInstanceOf(OrderMongoRepo);
+    });
+  });
+
+  describe('When create method is called', () => {
+    test('Then it should create an order', async () => {
+      (mockModel.create as jest.Mock).mockResolvedValue(mockOrder);
+
+      await repo.create(mockOrder);
+
+      expect(mockModel.create).toHaveBeenCalled();
+    });
+  });
+  describe('When update method is called', () => {
+    test('Then it should update an order', async () => {
+      (mockModel.findByIdAndUpdate as jest.Mock).mockResolvedValue(mockOrder);
+
+      await repo.update(mockOrder);
+
+      expect(mockModel.findByIdAndUpdate).toHaveBeenCalled();
+    });
+  });
+  describe('When find method is called', () => {
+    test('Then it should find an order', async () => {
+      (mockModel.findById as jest.Mock).mockImplementation(mockPopulateExec);
+
+      await repo.find('2');
+
+      expect(mockModel.findById).toHaveBeenCalled();
+    });
+  });
+  describe('When delete method is called', () => {
+    test('Then it should delete an order', async () => {
+      (mockModel.findByIdAndDelete as jest.Mock).mockResolvedValue(mockOrder);
+
+      await repo.delete('2');
+
+      expect(mockModel.findByIdAndDelete).toHaveBeenCalled();
+    });
+  });
+  describe('When search method is called', () => {
+    test('Then it should search an order', async () => {
+      (mockModel.find as jest.Mock).mockResolvedValue(mockOrder);
+
+      await repo.search({ key: 'state', value: 'in progress' });
+
+      expect(mockModel.find).toHaveBeenCalled();
+    });
+  });
+});
---
+++ kitchen/src/order/infrastructure/order.mongo.repo.ts
@@ -0,0 +1,36 @@
+import { OrderModel } from "../../server/domain/order.schema.js";
+import Order from "../domain/order.model.js";
+import OrderRepo from "../domain/order.model.repo.js";
+
+export default class OrderMongoRepo implements OrderRepo {
+  private orderModel: typeof OrderModel;
+
+  constructor(orderModel: typeof OrderModel) {
+    this.orderModel = orderModel;
+  }
+
+  async create(Order: Order): Promise<void> {
+    await this.orderModel.create(Order);
+  }
+
+  async update(Order: Partial<Order>): Promise<void> {
+    await this.orderModel.findByIdAndUpdate(Order.id, Order);
+  }
+
+  async find(id: string): Promise<Order> {
+    const response = await this.orderModel
+      .findById(id)
+      .populate('myGames', '-owner').populate('shopList', '-owner');
+    return response as Order;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.orderModel.findByIdAndDelete(id);
+  }
+
+  async search(query: { key: string; value: unknown }): Promise<Order[]> {
+    const response = await this.orderModel.find({ [query.key]: query.value });
+
+    return response;
+  }
+}
