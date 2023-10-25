28eed95eb85c022fbb9d1b4bcf4b7db3548aca08
Fabio <fabiodn.798@gmail.com>
2023-10-25 12:00:16 +0200

    ⚡️ Create order and dish router

--- kitchen/src/server/infrastructure/routers/dish.router.ts
+++ kitchen/src/server/infrastructure/routers/dish.router.ts
@@ -2,20 +2,21 @@
 import { Router } from "express";
 import { Interceptors } from "../../application/interceptors/interceptors.js";
 import ServerRouter from "../Server.router.interface.js";
-import { OrderController } from "../../application/controller/order.controller.js";
-export default class OrderRouter implements ServerRouter {
+import { DishController } from "../../application/controller/dish.controller.js";
 
-  path: string = '/order';
+export default class DishRouter implements ServerRouter {
+
+  path: string = '/dish';
   interceptor: Interceptors = new Interceptors();
   // eslint-disable-next-line new-cap
   router: Router = Router();
 
-  constructor(private controller: OrderController) {
+  constructor(private controller: DishController) {
     this.registerControllers();
   }
 
   registerControllers(): void {
-   this.router.post('/order', this.interceptor.dishInterceptor, this.controller.createOrder.bind(this.controller));
-   this.router.get('/history', this.controller.findAllOrder.bind(this.controller));
+   this.router.post('/dish', this.controller.createDish.bind(this.controller));
+   this.router.get('/dishes', this.controller.findAllDish.bind(this.controller));
   }
 }
