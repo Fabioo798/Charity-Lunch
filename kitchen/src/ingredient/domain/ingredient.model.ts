17031ba0d6c3654f915136407d02da5529e482bf
Fabio <fabiodn.798@gmail.com>
2023-10-25 12:06:53 +0200

    🚧  Create ingredient class

--- kitchen/src/ingredient/domain/ingredient.model.ts
+++ kitchen/src/ingredient/domain/ingredient.model.ts
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-useless-constructor */
 
 
 export default class Ingredient {
  constructor(
-  id: string,
-  name: string,
-  quantity: number,
- // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public name: string,
+  public quantity: number,
  ) {}
 }
