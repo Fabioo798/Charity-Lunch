abdf4c06f1e52276c69c07c106e8b065daf57998
Fabio <fabiodn.798@gmail.com>
2023-10-27 16:33:24 +0200

    update dish and order routers

--- kitchen/src/server/infrastructure/routers/dish.router.ts
+++ kitchen/src/server/infrastructure/routers/dish.router.ts
@@ -3,17 +3,21 @@ import { Router } from "express";
 import { Interceptors } from "../../application/interceptors/interceptors.js";
 import ServerRouter from "../Server.router.interface.js";
 import { DishController } from "../../application/controller/dish.controller.js";
+import OrderUpdater from "../../../order/application/orderupdater.js";
 
 export default class DishRouter implements ServerRouter {
 
+  constructor(private controller: DishController,
+  private orderUpdater: OrderUpdater) {
+   this.registerControllers();
+ }
+
   path: string = '/dish';
-  interceptor: Interceptors = new Interceptors();
+  interceptor: Interceptors = new Interceptors(this.orderUpdater);
   // eslint-disable-next-line new-cap
   router: Router = Router();
 
-  constructor(private controller: DishController) {
-    this.registerControllers();
-  }
+  
 
   registerControllers(): void {
    this.router.post('/dish', this.controller.createDish.bind(this.controller));
--- kitchen/src/server/infrastructure/routers/order.router.ts
+++ kitchen/src/server/infrastructure/routers/order.router.ts
@@ -3,19 +3,22 @@ import { Router } from "express";
 import { Interceptors } from "../../application/interceptors/interceptors.js";
 import ServerRouter from "../Server.router.interface.js";
 import { OrderController } from "../../application/controller/order.controller.js";
+import OrderUpdater from "../../../order/application/orderupdater.js";
 export default class OrderRouter implements ServerRouter {
+ constructor(private controller: OrderController,
+  private orderUpdater: OrderUpdater) {
+   this.registerControllers();
+ }
 
   path: string = '/order';
-  interceptor: Interceptors = new Interceptors();
+  interceptor: Interceptors = new Interceptors(this.orderUpdater);
   // eslint-disable-next-line new-cap
   router: Router = Router();
 
-  constructor(private controller: OrderController) {
-    this.registerControllers();
-  }
 
   registerControllers(): void {
-   this.router.post('/order', this.interceptor.dishInterceptor, this.controller.createOrder.bind(this.controller));
+   this.router.post('/create', this.interceptor.dishInterceptor, this.controller.createOrder.bind(this.controller), this.interceptor.dishInfoInterceptor.bind(this.controller));
    this.router.get('/history', this.controller.findAllOrder.bind(this.controller));
+   this.router.get('/state=:state', this.controller.searchOrder.bind(this.controller));
   }
 }
