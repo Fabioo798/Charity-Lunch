058d6eb8a79d698d7f70706db337765740fcf36e
Fabio <fabiodn.798@gmail.com>
2023-10-27 16:38:36 +0200

    🚀 Update order-history component

--- restaurant/src/app/order-history/order-history.component.ts
+++ restaurant/src/app/order-history/order-history.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { OrderService } from '../service/order.service/order.service';
+import { KitchenOrderResponse, KitchenOrdersArrResponse, Order } from 'src/interfaces/interfaces';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+
 
 @Component({
   selector: 'app-order-history',
@@ -6,5 +12,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-history.component.scss']
 })
 export class OrderHistoryComponent {
+ orders: Order[];
 
+  constructor(private orderService: OrderService) {
+   this.orders = [];
+   }
+
+  ngOnInit(): void {
+    this.loadOrderHistory();
+    registerLocaleData(localeEs, 'Es', localeEsExtra);
+
+  }
+
+  loadOrderHistory(): void {
+    this.orderService.orderHistory().subscribe({
+     next: (response: KitchenOrdersArrResponse) => {
+      this.orders = response.results;
+      console.log(this.orders)
+     },
+     error: (error) => {
+      console.log(error)
+     }
+  });
+  }
 }
