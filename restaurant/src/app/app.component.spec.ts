35d44910cce98689602590e76ed9b572b26864b3
Fabio <fabiodn.798@gmail.com>
2023-10-31 03:16:43 +0100

    ⚡️ Update app

--- restaurant/src/app/app.component.spec.ts
+++ restaurant/src/app/app.component.spec.ts
@@ -3,11 +3,13 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './infrastructure/layout/layout.component';
 import { HeaderComponent } from './infrastructure/header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, BrowserModule, HttpClientModule],
       declarations: [AppComponent, LayoutComponent, HeaderComponent],
     })
   );
