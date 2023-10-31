2b0cea4e035376c3096dcbfb706344b7be7446c8
Fabio <fabiodn.798@gmail.com>
2023-10-31 03:12:03 +0100

    ✅ test interceptors

---
+++ store/src/server/application/interceptors/interceptors.test.ts
@@ -0,0 +1,198 @@
+import { Request, Response, NextFunction } from 'express';
+import { Interceptors } from './interceptors'; // Note: replace with actual path
+import IngredientSearcher from '../../../Ingredients/application/ingredientsearcher.js';
+import IngredientUpdater from '../../../Ingredients/application/ingredientupdater.js';
+import Ingredient from '../../../Ingredients/domain/ingredient.model.js';
+import ShopListCreator from '../../../shopList/application/shoplistcreator.js';
+import IngredientRepo from '../../../Ingredients/domain/ingredient.model.repo.js';
+import ShopListRepo from '../../../shopList/domain/shopList.model.repo';
+import axios from 'axios';
+
+describe('Interceptors', () => {
+  let interceptor: Interceptors;
+  let mockIngredientSearcher: jest.Mocked<IngredientSearcher>;
+  let mockIngredientUpdater: jest.Mocked<IngredientUpdater>;
+  let mockShopListCreator: jest.Mocked<ShopListCreator>;
+
+  jest.mock('axios');
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  const mockRepo = {
+  update: jest.fn(),
+  create: jest.fn(),
+  find: jest.fn(),
+  findAll: jest.fn(),
+  delete: jest.fn(),
+  search: jest.fn(),
+
+} as unknown as IngredientRepo;
+
+ const shopListMockRepo = {
+  create: jest.fn()
+  
+ } as unknown as ShopListRepo
+
+  beforeEach(() => {
+    // Mock the classes
+    IngredientSearcher.prototype.execute = jest.fn();
+    mockIngredientSearcher =  new IngredientSearcher(mockRepo) as unknown as jest.Mocked<IngredientSearcher>;
+
+    IngredientUpdater.prototype.execute = jest.fn();
+    mockIngredientUpdater =  new IngredientUpdater(mockRepo) as unknown as jest.Mocked<IngredientUpdater>;
+
+    ShopListCreator.prototype.execute = jest.fn();
+    mockShopListCreator = new ShopListCreator(shopListMockRepo) as unknown as jest.Mocked<ShopListCreator>;
+
+    // Create the interceptor with the mock classes
+    interceptor = new Interceptors(mockIngredientSearcher, mockIngredientUpdater, mockShopListCreator);
+  });
+
+  test('firstMiddleware adds missingIngredients to req.body and calls next function', async () => {
+
+    const req = {
+      body: {
+        ingredients: [
+          { name: 'ingredient1', quantity: 10 },
+        ]
+      }
+    } as unknown as Request;
+
+    const res = {} as Response;
+    const next = jest.fn() as NextFunction;
+
+    const searchedIngredients: Ingredient[] = [
+      { id: '1', name: 'ingredient1', quantity: 5 }
+    ];
+
+    // Mock execute to return ingredients with insufficient quantities
+    mockIngredientSearcher.execute.mockResolvedValueOnce(Promise.resolve(searchedIngredients));
+
+    await interceptor.firstMiddleware(req, res, next);
+
+    expect(req.body.skip).toEqual(false);
+    expect(req.body.missingIngredients[0].name).toBe('ingredient1');
+    expect(next).toHaveBeenCalled();
+  });
+
+  // Continue with the initial setup, import statements, and existing tests...
+
+it('should catch errors and pass them to the next middleware', async () => {
+    
+  const req = {
+    body: {
+      ingredients: [
+        { name: 'ingredient1', quantity: 10 },
+      ]
+    }
+  } as unknown as Request;
+
+  const res = {} as Response;
+  const next = jest.fn() as NextFunction;
+
+  // Simulate an error by having `ingredientSearcher.execute` reject
+  mockIngredientSearcher.execute.mockRejectedValueOnce(new Error('Something went wrong.'));
+
+  await interceptor.firstMiddleware(req, res, next);
+
+  expect(next).toHaveBeenCalled();  
+});
+
+it('should not add missing ingredients when there are enough ingredients', async () => {
+    
+  const req = {
+    body: {
+      ingredients: [
+        { name: 'ingredient1', quantity: 10 },
+      ]
+    }
+  } as unknown as Request;
+
+  const res = {} as Response;
+  const next = jest.fn() as NextFunction;
+
+  const searchedIngredients: Ingredient[] = [
+    { id: '1', name: 'ingredient1', quantity: 20 }
+  ];
+
+  // `ingredientSearcher.execute` will resolve with enough ingredients
+  mockIngredientSearcher.execute.mockResolvedValueOnce(searchedIngredients);
+
+  await interceptor.firstMiddleware(req, res, next);
+
+  // Ensure no missing ingredients were added
+  expect(req.body.skip).toEqual(true);
+  expect('missingIngredients' in req.body).toBe(false);
+  expect(next).toHaveBeenCalled();
+  
+});
+
+  test('secondMiddleware fetches missing ingredients and adds results to req.body', async () => {
+    mockedAxios.get = jest.fn().mockResolvedValueOnce({ data: { quantitySold: 10 } }).mockResolvedValueOnce({ data: { quantitySold: 20 } });
+    const req = {
+      body: {
+        skip: false,
+        missingIngredients: [{ id: 1, name: 'ingredient1', quantity: 10 }],
+      }
+    } as unknown as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(), 
+      json: jest.fn()
+    } as unknown as Response;
+    const next = jest.fn() as NextFunction;
+
+    // Snapshot of axios response for '/farmers-market/buy'
+    const axiosResponse = { data: { quantitySold: 10 } };
+
+    // Mock axios.get to return quantitySold
+    mockedAxios.get.mockResolvedValueOnce(axiosResponse);
+
+    await interceptor.secondMiddleware(req, res, next);
+
+    expect(req.body.results).toBeTruthy()
+    expect(next).toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+  });
+
+  test('thirdMiddleware updates ingredients and calls next function', async () => {
+
+    const req = {
+      body: {
+        skip: false,
+        results: [{ id: 1, name: 'ingredient1', quantity: 10 }],
+      },
+    } as unknown as Request;
+    const res = {} as Response;
+    const next = jest.fn() as NextFunction;
+
+    await interceptor.thirdMiddleware(req, res, next);
+
+    expect(mockIngredientUpdater.execute).toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
+
+   test('fourthMiddleware updates ingredients and send success response', async () => {
+
+    const req = {
+      body: {
+        ingredients: [{ id: 1, name: 'ingredient1', quantity: 10 }],
+      },
+    } as unknown as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(), 
+      json: jest.fn(),
+    } as unknown as Response;
+    
+    const next = jest.fn();
+
+    // Mock ingredientSearcher.execute to return ingredientInDbData
+    mockIngredientSearcher.execute.mockResolvedValue([{ id: '1', name: 'ingredient1', quantity: 10 }]);
+
+    await interceptor.fourthMiddleware(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalled();
+    expect(next).not.toHaveBeenCalled();
+  });
+});
--- store/src/server/application/interceptors/interceptors.ts
+++ store/src/server/application/interceptors/interceptors.ts
@@ -6,8 +6,11 @@ import axios from "axios";
 import IngredientUpdater from "../../../Ingredients/application/ingredientupdater.js";
 import { HTTPError } from "../../../common/error.js";
 import ShopList from "../../../shopList/domain/shopList.model.js";
-import ShopListCreator from "../../../shopList/application/shopListCreator.js";
+import ShopListCreator from "../../../shopList/application/shoplistcreator.js";
+import createDebug from 'debug';
 
+
+const debug = createDebug('CL-Store');
 
 export class Interceptors {
  // eslint-disable-next-line no-useless-constructor
@@ -21,12 +24,11 @@ export class Interceptors {
 
  async firstMiddleware(req: Request, res: Response, next: NextFunction) {
   try {
+   debug('First middleware called')
     let skipSecondAndThird: boolean;
     // eslint-disable-next-line prefer-destructuring
     const  ingredients  = req.body.ingredients;
-    console.log('INGREDIENT IN THE BODY' , ingredients)
     const missingIngredients: Ingredient[] = [];
-    const availableIngredients: Ingredient[] = [];
 
     for (const ingredient of ingredients) {
       const [foundIngredient] = (await this.ingredientSearcher.execute(ingredient.name))
@@ -36,54 +38,45 @@ export class Interceptors {
         const requestedQuantity = ingredient.quantity;
 
       // Check guard for updating the quantity
-      if (currentQuantity < requestedQuantity) {
+      if (currentQuantity < requestedQuantity)
         missingIngredients.push({ id, name, quantity: requestedQuantity });
-      } else {
-        availableIngredients.push({ id, name, quantity: requestedQuantity });
-      }
     }
 
     if (missingIngredients.length > 0) {
       // Call the next middleware to handle the API call for insufficient quantity
       skipSecondAndThird = false
-      console.log("FIRST MIDDLWARE - NOT ENOUGH INGRED.")
+      debug('Not enough ingredient')
       req.body.skip = skipSecondAndThird;
       req.body.missingIngredients = missingIngredients;
       return next();
     }
     
-    console.log("FIRST MIDDLWARE - ENOUGH INGR");
-
+    debug("Enough ingredient");
     skipSecondAndThird = true;
     req.body.skip = skipSecondAndThird
-    console.log('CLG DE REQ' , req.body.skip)
     next();
 
   } catch (error) {
-      console.log('PRIMO MIDL ERROR!');
+      debug('Error in the first middleware')
       next(new HTTPError(500, 'Internal Server Error', 'Something went wrong.', error as Error));
     }
  }
  
   async secondMiddleware(req: Request, res: Response, next: NextFunction) {
   try {
+   debug('Second middleware called')
    const skipSecondAndThird = req.body.skip
    if(skipSecondAndThird === true){
-    console.log('SECOND MID - ENOUGH ING');
+    debug('Enough ingredient -> next()')
     next();
    }
    
-   console.log('SECOND MID - NOT ENOUGH INGR')
     // eslint-disable-next-line prefer-destructuring
     const missingIngredients = req.body.missingIngredients;
   
     // Store the promises for each API request
     const promises = missingIngredients.map(async (ingredient: Ingredient) => {
-      const response = await axios.get(`https://recruitment.alegra.com/api/farmers-market/buy?ingredient=${ingredient.name.toLocaleLowerCase()}`);
-      console.log('ALEGRA API', response.data);
-      
-      // eslint-disable-next-line no-debugger
-      debugger;
+      const response = await axios.get(`https://recruitment.alegra.com/api/farmers-market/buy?ingredient=${ingredient.name.toLocaleLowerCase()}`);      
       
       await this.shopListCreator.execute({name: ingredient.name, quantitySold: response.data.quantitySold, timeStamp: new Date()} as ShopList);
       return {
@@ -93,58 +86,54 @@ export class Interceptors {
       } as Ingredient;
     });
   
-    // Wait for all promises to resolve
     const results = await Promise.all(promises);
 
     const hasZeroQuantity = results.some((ingredient: Ingredient) => ingredient.quantity === 0);
 
     if (hasZeroQuantity) {
+      debug('Not enough ingredient from market!');
       return res.status(400).json({ message: "Not enough ingredients" });
     }
 
     // Pass the results to the next middleware or send the response as needed...
     req.body.results = results;
-    console.log('SECOND MID - ALEGRA FETCH SUCCESS' + results)
     return next();
   
   } catch (error) {
-      console.log('SECONDO MIDL ERROR!');
+      debug('Second middleware error');
       next(new HTTPError(500, 'Internal Server Error', 'Something went wrong.'));
     }
  }
 
  async thirdMiddleware(req: Request, res: Response, next: NextFunction) {
   try {
+   debug('third middleware called')
    const skipSecondAndThird = req.body.skip
    if(skipSecondAndThird === true){
-    console.log('THIRD MIDDL - ENOUGH INGR')
+    debug('enough ingredient')
     next();
    }
 
     const promises  = req.body.results;
 
-    // Store the promises for updating the quantities in the database
     const updatePromises = promises.map(async (ingredient: Ingredient) => {
-      // Call the updateIngredient method to update the specific ingredient
       await this.ingredientUpdater.execute(ingredient.id, {quantity: ingredient.quantity});
     });
 
-    // Wait for all promises to resolve
     await Promise.all(updatePromises);
-     console.log('THIRD MIDL - ALL INGRED UPDATED');
-    // Call the next middleware or send the response accordingly
+    debug('Ingredient updated')
     return next();
   } catch (error) {
-      console.log('TERZO MIDL ERROR!');
+     debug('Error in the third middleware')
       next(new HTTPError(500, 'Internal Server Error', 'Something went wrong.', error as Error));
     }
 }
 
   async fourthMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
+      debug('Fourth middleware called')
       // eslint-disable-next-line prefer-destructuring
       const  ingredients: Ingredient[]  = req.body.ingredients;
-     console.log('4 MIDLW INGRED', ingredients)
 
      
       const promises = [];
@@ -152,15 +141,14 @@ export class Interceptors {
         const ingredientInDbData = await this.ingredientSearcher.execute([ingredient.name]);
         const newQuantity = ingredientInDbData[0].quantity - ingredient.quantity;
         await this.ingredientUpdater.execute(ingredientInDbData[0].id, { quantity: newQuantity });
-        console.log('INGREDIENTUPDATE EXECUTED');
         promises.push(ingredient);
-}
+       }
 
       await Promise.all(promises);
-      console.log("PROCESS COMPLETED SUCCESFULLY")
+      debug('Process completed, ingredients updated')
       return res.status(200).json({ message: `The Ingredients ${ingredients} updated successfully` });
     } catch (error) {
-      console.log('QUARTO MIDL ERROR!');
+      debug('Error in the fourth middleware');
       next(new HTTPError(500, 'Internal Server Error', 'Something went wrong.', error as Error));
     }
 }
